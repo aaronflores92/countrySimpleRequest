1a0e41206e58e9c3a74fd19f7a0b6fade95975a4
aaronflores92 <31676547+aaronflores92@users.noreply.github.com>
2024-05-01 15:58:01 -0700

    refactor Main app class and add logging to class
    Refactor the Main app class to remove packages/modules which are no longer needed in the class (moved to CountryController.java). Add logging capability to Main app class to log start messages in the console.

--- src/main/java/com/restService/countryDetails/CountryDetailsApplication.java
+++ src/main/java/com/restService/countryDetails/CountryDetailsApplication.java
@@ -1,23 +1,19 @@
 package com.restService.countryDetails;
 
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.core.ParameterizedTypeReference;
 
 @SpringBootApplication
 public class CountryDetailsApplication {
+	
+	// Logger to send output to Console
+	private static final Logger log = LoggerFactory.getLogger(CountryDetailsApplication.class);
 
 	public static void main(String[] args) {
+		String countryURI = "https://restcountries.com/v3.1/name/";
 		SpringApplication.run(CountryDetailsApplication.class, args);
+		log.info("GET operation against " + countryURI + ". Use countryName as query param, defaults to using USA.");
 	}
 }
